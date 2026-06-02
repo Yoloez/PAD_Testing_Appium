ffbdbc9e6c4948a8814581aed7f359ecc3f8e222
hanan <hananfijananto@gmail.com>
2026-06-02 09:41:44 +0700

    feat: add readme for setup appium

--- src/main/java/org/example/locators/MatkulLocators.java
+++ src/main/java/org/example/locators/MatkulLocators.java
@@ -6,10 +6,10 @@ public final class MatkulLocators {
 
     public static final By MENU_MATKUL = By.id("menu_matkul");
     public static final By BTN_TAMBAH = By.id("btn_add");
-    public static final By INPUT_NAMA = By.id("et_nama_matkul");
-    public static final By INPUT_KODE = By.id("et_kode_matkul");
-    public static final By INPUT_SKS = By.id("et_sks");
-    public static final By BTN_SIMPAN = By.id("btn_simpan");
+    public static final By INPUT_NAMA = By.id("input-name-create-subject");
+    public static final By INPUT_KODE = By.id("input-code-create-subject");
+    public static final By INPUT_SKS = By.id("input-sks-create-subject");
+    public static final By BTN_SIMPAN = By.id("btn-save-create-subject");
     public static final By BTN_EDIT = By.id("btn_edit");
     public static final By ALERT_MESSAGE = By.id("tv_alert_message");
     public static final By ALERT_OK = By.id("btn_ok");
