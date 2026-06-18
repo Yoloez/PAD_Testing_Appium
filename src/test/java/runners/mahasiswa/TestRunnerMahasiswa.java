13ee2006b4a4b79cb5a8e68eba71010515621d88
BabbyBrown03 <revaldousskuncoroputra@gmail.com>
2026-06-19 06:06:57 +0700

    update LecturerPage.java, LecturerSteps.java, lecturer.feature, TestRunnerLecturer.java

--- src/test/java/runners/mahasiswa/TestRunnerMahasiswa.java
+++ src/test/java/runners/mahasiswa/TestRunnerMahasiswa.java
@@ -1,18 +1,18 @@
-package runners.mahasiswa;
-
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        features = "src/test/resources/kelola_mahasiswa.feature",
-        glue = {"stepDef"},
-        plugin = {
-                "pretty",
-                "html:target/cucumber-reports/mahasiswa-report.html"
-        },
-        monochrome = true
-)
-public class TestRunnerMahasiswa {
-}
+//package runners.mahasiswa;
+//
+//import io.cucumber.junit.Cucumber;
+//import io.cucumber.junit.CucumberOptions;
+//import org.junit.runner.RunWith;
+//
+//@RunWith(Cucumber.class)
+//@CucumberOptions(
+//        features = "src/test/resources/kelola_mahasiswa.feature",
+//        glue = {"stepDef"},
+//        plugin = {
+//                "pretty",
+//                "html:target/cucumber-reports/mahasiswa-report.html"
+//        },
+//        monochrome = true
+//)
+//public class TestRunnerMahasiswa {
+//}
